1e6f92525bae5eea17863822e71b8e1e7aebb6da
Nasyih Ulwan <muhnasyihulwan@gmail.com>
2025-06-06 21:05:25 +0700

    first commit

---
+++ src/main/java/com/mycompany/tugbes_pbo/TUGBES_PBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugbes_pbo;
+
+/**
+ *
+ * @author nasyihulwan
+ */
+public class TUGBES_PBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
